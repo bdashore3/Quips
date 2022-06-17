0458c66d7a394df1f808031c703b557a1b6cc5fd
kingbri <bdashore3@gmail.com>
2022-06-16 21:10:55 -0400

    Initial Commit

---
+++ Quips/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Quips
+//
+//  Created by Brian Dashore on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quips/QuipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuipsApp.swift
+//  Quips
+//
+//  Created by Brian Dashore on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct QuipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
